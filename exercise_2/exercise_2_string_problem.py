a408187506c3d90111e9f8999939e550dbcce514
pritesh604 <priteshbhimani95@gmail.com>
2023-10-20 09:15:19 +0200

    new exercise

---
+++ exercise_2/exercise_2_string_problem.py
@@ -0,0 +1,11 @@
+notes = "Cucumber, Please remember to buuy the following things: \ Fish, Shell Polish, Fish, Slippers."
+
+print(len(notes))
+
+print(notes[:29]+"buy"+notes[33:])
+
+print(notes[:77] + notes[83:93])
+
+print(notes[10:92] + ", cucumber.")
+
+print(notes[58:92] + ", cucumber.")
